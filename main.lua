395dfd8524feca43eb53a2c5ccb4185213a6bd9e
kangmin <kangmin.kim@ocsbstudent.ca>
2018-02-15 13:01:31 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,21 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- created by: kangmin
+-- created by: Feb 15 2018
+-----------------------------------------------------------------------------------------
+
+local soccerBall = display.newImageRect( "./assets/sprites/ball.png", 500, 480)
+soccerBall.x = display.contentCenterX
+soccerBall.y = display.contentCenterY
+soccerBall.id = "ball object"
+
+local function onObjectTouch( event )
+	-- it is not perfect, but we will make it better soon
+    print( "Touched ball ")
+
+    return true
+end
+
+soccerBall:addEventListener( "touch", onObjectTouch )
